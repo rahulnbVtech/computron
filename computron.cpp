eaf4038a4582e6baee13c553c29e5ba0da96518f
rahulnbVtech <143011976+rahulnbVtech@users.noreply.github.com>
2025-03-03 18:36:42 -0500

    Add files via upload

--- computron.cpp
+++ computron.cpp
@@ -82,7 +82,7 @@ void execute(std::array<int, memorySize>& memory, int* const acPtr, size_t* cons
             break;
 
         case Command::load:
-            memory[*opPtr] = *acPtr;
+            *acPtr = memory[*opPtr];
             (*icPtr)++;
             break;
 
